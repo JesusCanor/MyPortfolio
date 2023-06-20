0733c062270f8cf4132ff6230403e810eda58a8f
jesus <jesuscanorodriguez@outlook.com>
2023-06-20 13:28:19 +0200

    Dummy test to fix

---
+++ src/test/App.test.jsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+
+describe('something truthy and falsy', () => {
+    it('true to be true', () => {
+      expect(true).toBe(true);
+    });
+});
+
+// test('renders page', () => {
+//     render(<App />);
+// });
+//   it('false to be false', () => {
+//     expect(false).toBe(false);
+//   });
---
+++ src/test/vitest.config.ts
@@ -0,0 +1,8 @@
+export default {
+    test: {
+        environment: 'happy-dom',
+        deps: {
+            inline: ["clsx", "countup.js"]
+      } 
+    }
+}
